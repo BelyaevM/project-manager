ecf2eadcad3f24c4f39968adf163403afef5fd38
BelyaevM <belyaev_mp@bk.ru>
2018-04-21 11:05:17 +0400

    Create index controller.

---
+++ src/main/java/com/bmp/projectmanager/application/web/controller/IndexController.java
@@ -0,0 +1,20 @@
+package com.bmp.projectmanager.application.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class IndexController {
+
+
+    @GetMapping("/")
+    public ModelAndView index(ModelAndView model) {
+
+        model.addObject("name", "User");
+        model.setViewName("index");
+
+        return model;
+    }
+
+}
---
+++ src/main/webapp/WEB-INF/jsp/index.jsp
@@ -0,0 +1,7 @@
+<html>
+
+<body>
+<h2>Hello ${name }</h2>
+</body>
+
+</html>
